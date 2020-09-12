c8be1ae431e5c9fa0e5e91c11319b49bf61722ad
Çağatay Sarıoğlu <69264926+cagataysarioglu@users.noreply.github.com>
2020-09-12 21:09:47 +0300

    Update film_arsivi_api_uygulamasi.py

--- film_arsivi_api_uygulamasi.py
+++ film_arsivi_api_uygulamasi.py
@@ -2,8 +2,8 @@ import requests
 
 class filmArsivKaynagi:
     def __init__(self):
-        self.api_url = "https://api.themoviedb.org/3"
-        self.api_anahtar = "78dfhoiw343uhfuf94uhqewy457"
+        self.api_url = "https://......."
+        self.api_anahtar = "......."
     def populerleriGetir(self):
         istek = requests.get(f"{self.api_url}/movie/popular?api_key={self.api_anahtar}&languge=tr-TR&page=1")
         return istek.json()
